72bd88fb4c44f4597b6633b30cae5c384697eed3
Lazizbek6766 <turayevlazizbek6766@gmail.com>
2024-01-24 17:02:59 +0500

    ui

--- app/src/main/java/com/example/fakestoreapi/presentation/screen/home/components/CategoryLazyRow.kt
+++ app/src/main/java/com/example/fakestoreapi/presentation/screen/home/components/CategoryLazyRow.kt
@@ -3,6 +3,8 @@ package com.example.fakestoreapi.presentation.screen.home.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,58 +15,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CategoryLazyRow(
     data: List<String>,
     selectedText: String,
     changeText: (String) -> Unit,
-    isLoading:Boolean
+    isLoading: Boolean
 ) {
     LazyRow(content = {
-        if(isLoading){
+        if (isLoading) {
             items(4) { item ->
-                if (item == 0) {
-                    Card(
-                        modifier = Modifier
-                            .padding(vertical = 4.dp, horizontal = 8.dp),
-                        border = BorderStroke(1.dp, Color.Black)
-                    ) {
-                        Text(
-                            text = "",
-                            modifier = Modifier
-                                .shimmerBackground1(RoundedCornerShape(10.dp))
-                                .background(Color.Black)
-                                .width(100.dp)
-                                .padding(vertical = 6.dp, horizontal = 8.dp),
-                            color = Color.White
-                        )
-                    }
-                } else {
-                    Card(
-                        modifier = Modifier
-                            .padding(vertical = 4.dp, horizontal = 8.dp),
-                        border = BorderStroke(1.dp, Color.Black)
-                    ) {
-                        Text(
-                            text = "",
-                            modifier = Modifier
-                                .shimmerBackground1(RoundedCornerShape(10.dp))
-                                .background(Color.White)
-                                .width(100.dp)
-                                .padding(vertical = 6.dp, horizontal = 8.dp),
-                            color = Color.Black
-                        )
-                    }
-                }
+                Box(
+                    modifier = Modifier
+                        .padding(vertical = 4.dp, horizontal = 8.dp)
+                        .width(100.dp)
+                        .height(32.dp)
+                        .shimmerBackground1(RoundedCornerShape(16.dp)),
+                )
             }
-        }else{
+        } else {
             itemsIndexed(data) { index, item ->
                 if (item == selectedText) {
                     Card(
                         modifier = Modifier
-                            .padding(vertical = 4.dp, horizontal = 8.dp),
+                            .padding(vertical = 4.dp, horizontal = 4.dp),
                         border = BorderStroke(1.dp, Color.Black),
                         shape = RoundedCornerShape(16.dp)
                     ) {
@@ -72,14 +51,20 @@ fun CategoryLazyRow(
                             text = item,
                             modifier = Modifier
                                 .background(Color.Black)
-                                .padding(vertical = 4.dp, horizontal = 8.dp),
-                            color = Color.White
+                                .padding(vertical = 4.dp, horizontal = 12.dp),
+                            color = Color.White,
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                fontWeight = FontWeight(400)
+
+                            )
                         )
                     }
                 } else {
                     Card(
                         modifier = Modifier
-                            .padding(vertical = 4.dp, horizontal = 8.dp),
+                            .padding(vertical = 4.dp, horizontal = 4.dp),
                         border = BorderStroke(1.dp, Color.Black),
                         shape = RoundedCornerShape(16.dp)
                     ) {
@@ -87,9 +72,15 @@ fun CategoryLazyRow(
                             text = item,
                             modifier = Modifier
                                 .background(Color.White)
-                                .padding(vertical = 4.dp, horizontal = 8.dp)
+                                .padding(vertical = 4.dp, horizontal = 12.dp)
                                 .clickable { changeText(item) },
-                            color = Color.Black
+                            color = Color.Black,
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                fontWeight = FontWeight(400)
+
+                            )
                         )
                     }
                 }
--- app/src/main/java/com/example/fakestoreapi/presentation/screen/home/components/ShimmerItem.kt
+++ app/src/main/java/com/example/fakestoreapi/presentation/screen/home/components/ShimmerItem.kt
@@ -1,6 +1,5 @@
 package com.example.fakestoreapi.presentation.screen.home.components
 
-import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
@@ -11,49 +10,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.fakestoreapi.R
-import com.example.fakestoreapi.domain.model.AllProductItem
-import com.example.fakestoreapi.domain.model.Rating
-import com.example.fakestoreapi.ui.theme.Pink50
-
 
 @Composable
 fun ShimmerListItem(
@@ -121,31 +95,31 @@ fun Modifier.shimmerBackground1(shape: Shape = RectangleShape): Modifier = compo
     return@composed this.then(background(brush, shape))
 }
 
-fun Modifier.shimmerBackground(shape: Shape = RectangleShape): Modifier = composed {
-    var size by remember {
-        mutableStateOf(IntSize.Zero)
-    }
-    val transition = rememberInfiniteTransition()
-    val startOffsetX by transition.animateFloat(
-        initialValue = -2 * size.width.toFloat(),
-        targetValue = 2 * size.width.toFloat(),
-        animationSpec = infiniteRepeatable(
-            animation = tween(1000)
-        )
-    )
-
-    background(
-        brush = Brush.linearGradient(
-            colors = listOf(
-                Color(0xFFB8B5B5),
-                Color(0xFF8F8B8B),
-                Color(0xFFB8B5B5),
-            ),
-            start = Offset(startOffsetX, 0f),
-            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
-        )
-    )
-        .onGloballyPositioned {
-            size = it.size
-        }
-}
+//fun Modifier.shimmerBackground(shape: Shape = RectangleShape): Modifier = composed {
+//    var size by remember {
+//        mutableStateOf(IntSize.Zero)
+//    }
+//    val transition = rememberInfiniteTransition()
+//    val startOffsetX by transition.animateFloat(
+//        initialValue = -2 * size.width.toFloat(),
+//        targetValue = 2 * size.width.toFloat(),
+//        animationSpec = infiniteRepeatable(
+//            animation = tween(1000)
+//        )
+//    )
+//
+//    background(
+//        brush = Brush.linearGradient(
+//            colors = listOf(
+//                Color(0xFFB8B5B5),
+//                Color(0xFF8F8B8B),
+//                Color(0xFFB8B5B5),
+//            ),
+//            start = Offset(startOffsetX, 0f),
+//            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+//        )
+//    )
+//        .onGloballyPositioned {
+//            size = it.size
+//        }
+//}
